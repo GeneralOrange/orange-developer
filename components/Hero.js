0467bdf8d4d2d2c08ff2f0dfa9583be88d79a8c1
General Orange <erik.17021995@gmail.com>
2022-04-14 00:20:50 +0200

    Implemented typedJS and hero changes

---
+++ components/Hero.js
@@ -0,0 +1,58 @@
+import React, { useRef, useEffect } from 'react'
+import Image from 'next/image'
+import Typed from 'typed.js'
+import styles from '../styles/Hero.module.scss'
+
+export default function Hero() {
+
+    const el = useRef(null);
+    const typed = useRef(null);
+
+    useEffect(()=> {
+        const options = {
+            strings: [
+                '<span style="color: rgb(45, 45, 45)">Leuke</span> ^200Developer',
+                '<span style="color: rgb(45, 45, 45)">Creatieve</span> ^200Developer',
+                '<span style="color: rgb(45, 45, 45)">Front-end</span> ^200Developer'
+            ],
+            typeSpeed: 150,
+            backSpeed: 100
+        };
+
+        typed.current = new Typed(el.current, options);
+
+        return () => {
+            typed.current.destroy();
+        }
+    }, [])
+
+    return (
+        <div className={styles.hero}>
+            <div className='container'>
+                <div className={styles.heroWrapper}>
+                    <div className={styles.section}>
+                        <Image
+                            src="/profile.png"
+                            width="434"
+                            height="575"/>
+                    </div>
+                    <div className={styles.section}>
+                        <Image
+                            className={styles.logo}
+                            src="/generalOrangeLogo.png"
+                            width="50"
+                            height="50"/>
+                        <span className={styles.subtitle}>Hallo, mijn naam is Erik</span>
+                        <h1 className={styles.title}>
+                            Ik ben een <br/> <span ref={el}/>
+                        </h1>
+                        <button
+                            className={styles.button}>
+                            Contact
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
