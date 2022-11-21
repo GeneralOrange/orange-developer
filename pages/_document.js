c56352204ba5ea584ee5bbee642de778f7a6bb92
General Orange <erik.17021995@gmail.com>
2022-11-21 22:57:06 +0100

    Added Google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,34 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { GA_TRACKING_ID } from '../lib/gtag'
 
-class MyDocument extends Document {
+export default class MyDocument extends Document {
   render() {
+    
     return (
-      <Html>
+      <Html lang="nl">
         <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com"/>
           <link
             href="https://fonts.googleapis.com/css2?family=Mukta&display=swap"
             rel="stylesheet"
           />
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />     
         </Head>
         <body>
           <Main />
@@ -18,5 +38,3 @@ class MyDocument extends Document {
     )
   }
 }
-
-export default MyDocument
